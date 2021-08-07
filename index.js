1377737f00d88e35600c490262b6d2134df32559
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-08-07 23:31:54 +0530

    Users join kinda done

--- index.js
+++ index.js
@@ -7,67 +7,91 @@ const server = http.createServer(app);
 const socketio = require("socket.io");
 
 app.use(
-	CORS({
-		origin: "*",
-	})
+  CORS({
+    origin: "*",
+  })
 );
 const io = socketio(server, {
-	cors: {
-		origin: "*",
-	},
+  cors: {
+    origin: "*",
+  },
 });
 
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
 });
 app.get("/", (req, res) => {
-	res.send("Hello World");
+  res.send("Hello World");
 });
 
 let rooms = [];
+
 server.listen(PORT, () => {
-	console.log("Listening on port ", PORT);
+  console.log("Listening on port ", PORT);
 });
 io.on("connection", (socket) => {
-	socket.on("new", ([name, room]) => {
-		if (rooms.length === 0) {
-			rooms.push([room, [name]]);
-			socket.emit("room-info", rooms[0][1]);
-			socket.join(room);
-			socket.to(room).emit("new-user", [name, rooms[0][1]]);
-		} else {
-			let useroom = rooms.map((eachroom) => eachroom[0] === room);
-			let index = useroom.indexOf(true);
-			if (index !== -1) {
-				rooms[index][1].push(name);
-				socket.join(rooms[index][0]);
-				socket.to(rooms[index][0]).emit("new-user", [name, rooms[index][1]]);
-				socket.emit("room-info", rooms[index][1]);
-			} else {
-				rooms.push([room, [name]]);
-				socket.emit("room-info", rooms[rooms.length - 1][1]);
-				socket.join(room);
-				socket.to(room).emit("new-user", [rooms[0][0], rooms[0][1]]);
-			}
-			//  [[] , []]
-		}
-		socket.on("message", ([room, value, author, created_at]) => {
-			socket.to(room).emit("foreign-message", [value, author, created_at]);
-		});
-		socket.on("disconnect", () => {
-			let useroom = rooms.map((eachroom) => eachroom[0] === room);
-			let index = useroom.indexOf(true);
-			rooms[index][1].splice(rooms[index][1].indexOf(name), 1);
-			socket.to(rooms[index][0]).emit("user-left", [name, rooms[index][1]]);
-		});
-	});
-	socket.on("req-info", () => socket.emit("res-info", rooms));
+  console.log("A new client has connected!");
+  socket.on("new-user", (payload) => {
+    if (rooms.length === 0) {
+      rooms.push({
+        name: payload.roomName,
+        members: [
+          {
+            name: payload.name,
+            profilePic: payload.profilePic,
+          },
+        ],
+      });
+      socket.emit("room-info", rooms[0].members);
+      socket.join(payload.roomName);
+    } else {
+      const doesRoomExist = rooms.find((r) => r.name === payload.roomName);
+
+      if (doesRoomExist) {
+        console.log("Room already exists");
+        doesRoomExist.members.push({
+          name: payload.name,
+          profilePic: payload.profilePic,
+        });
+        socket.join(payload.roomName);
+        socket.to(payload.roomName).emit("room-info", doesRoomExist.members);
+        socket.emit("room-info", doesRoomExist.members);
+      }
+    }
+    socket.on("disconnect", () => {
+      const userRoom = rooms.find((r) => r.name === payload.roomName);
+      const filteredUsers = userRoom.members.filter(
+        (user) => user.name !== payload.name
+      );
+      userRoom.members = filteredUsers;
+      socket
+        .to(payload.roomName)
+        .emit("user-left", payload.name, userRoom.members);
+    });
+  });
 });
+
+/* 
+
+
+{
+	name:roomName , 
+	members:  [
+		{ name: userName , profilePic:<svg></svg>}
+	]
+}
+
+*/
+
 setInterval(() => {
-	rooms.forEach((room, index) => {
-		if (room[1].length === 0) {
-			rooms.splice(index, 1);
-		}
-	});
+  console.log(rooms);
+}, 2000);
+
+setInterval(() => {
+  rooms.forEach((room, index) => {
+    if (room.members.length === 0) {
+      rooms.splice(index, 1);
+    }
+  });
 }, 1000);
